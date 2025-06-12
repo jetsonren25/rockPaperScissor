1048091f88fc1ace75d33f2bbb613de8a834965d
Javone <javosmith@indigoempirellc.com>
2025-06-12 09:49:25 -0400

    removed comments

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
 /** 
- Problem:
- * Write a program that plays rock, paper, scissors. 
- * Your game will be played against the computer. 
- * You will write a function that randomly returns “rock”, “paper” or “scissors”.
- * Your game will be played by a human player. 
- * You will write a function that takes the user choice and returns it.
- * Your game will keep track of the players score. 
- * You will write variables to keep track of the players score.
- * Your game will be played round by round. 
- * You will write a function that takes the human and computer player choices as arguments, plays a single round, increments the round winner’s score and logs a winner announcement
- * Your game will play 5 rounds. 
- * You will write a function named playGame that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
- Planning: 
- * user input is needed.
  Pseudocode:
  * get a program to select a random rock, paper, or scissor 
  * get the user input to select rock, paper, or scissor and keep track of their score too
  * compare both the user and program selections to see who wins
- * --> 
- 
- * rock beats scissors
- * rock loses to paper
- 
- * paper beats rock
- * paper loses to scissors
- 
- * scissors beats paper
- * scissors loses to rock
- 
- * keep playing the game until the game has been played 5 times
- * 
- * declare a winner
  */
 
 
