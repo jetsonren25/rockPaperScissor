daa3fa90ba43724af14a8e0c31ed1299dc9c1a76
Javone <javosmith@indigoempirellc.com>
2025-06-24 14:35:01 -0400

    removed function for human input on rps and created btns for selection

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-/** 
- Pseudocode:
- * get a program to select a random rock, paper, or scissor 
- * get the user input to select rock, paper, or scissor and keep track of their score too
- * compare both the user and program selections to see who wins
- */
+let body = document.querySelector('body')
+
+let rock = document.createElement('button')
+rock.setAttribute('class','game')
+rock.textContent = 'rock'
+
+let paper = document.createElement('button')
+paper.setAttribute('class','game')
+paper.textContent = 'paper'
+
+let scissors = document.createElement('button')
+scissors.setAttribute('class','game')
+scissors.textContent = 'scissors'
+
+body.appendChild(rock)
+body.appendChild(paper)
+body.appendChild(scissors)
+
+// --------- CODE HERE
+const btns = document.querySelectorAll(".game")
+btns.forEach(btn => {
+    btn.addEventListener('click', (e) =>{
+        playRound(e.target.textContent)
+    })
+})
 
 
-let humanScore = 0
-let compScore = 0
-let rounds = 5
+
+// let humanScore = 0
+// let compScore = 0
+// // let rounds = 5
 
 const getCompChoice = function(){
     let options = ["rock","paper","scissors"]
@@ -17,50 +37,44 @@ const getCompChoice = function(){
     return choice
 }
 
-const getHumanChoice = function(){
-    let userInput = prompt(`Please type rock, paper, or scissors`)
-    return userInput
-}
+// function playRound(computerChoice,humanChoice){
+//     // let lowerCaseHumanChoice = humanChoice.toLowerCase()
+//     if(lowerCaseHumanChoice === computerChoice){
+//         return 'tie'
+//     } else if (lowerCaseHumanChoice === 'rock' && computerChoice === 'scissors' || 
+//         lowerCaseHumanChoice === 'paper' && computerChoice === 'rock'){
+//         return 'human'
+//     } else if(lowerCaseHumanChoice === 'scissors' && computerChoice === 'paper'){
+//         return 'human' 
+//     } else {
+//         return 'comp'
+//     }
+// }
 
-function playRound(computerChoice,humanChoice){
-    let lowerCaseHumanChoice = humanChoice.toLowerCase()
-    console.log(humanChoice)
-    if(lowerCaseHumanChoice === computerChoice){
-        return 'tie'
-    } else if (lowerCaseHumanChoice === 'rock' && computerChoice === 'scissors' || 
-        lowerCaseHumanChoice === 'paper' && computerChoice === 'rock'){
-        return 'human'
-    } else if(lowerCaseHumanChoice === 'scissors' && computerChoice === 'paper'){
-        return 'human' 
-    } else {
-        return 'comp'
-    }
-}
+// playGame = (isWinner) => {
+//     while(rounds > 1){
+//         switch (isWinner) {
+//             case 'tie':
+//                 alert('Tie')
+//                 break;
+//             case 'human':
+//                 alert('Human victory')
+//                 humanScore++
+//                 break;
+//             case 'comp':
+//                 alert('Sentient victory')
+//                 compScore++
+//                 break;
+//             default:
+//                 break;
+//             }
+//         rounds--
+//         playGame(playRound(getCompChoice(),getHumanChoice()))
+//     }
 
-playGame = (isWinner) => {
-    while(rounds > 1){
-        switch (isWinner) {
-            case 'tie':
-                alert('Tie')
-                break;
-            case 'human':
-                alert('Human victory')
-                humanScore++
-                break;
-            case 'comp':
-                alert('Sentient victory')
-                compScore++
-                break;
-            default:
-                break;
-            }
-        rounds--
-        playGame(playRound(getCompChoice(),getHumanChoice()))
-    }
+// }
 
-}
-
-playGame(playRound(getCompChoice(),getHumanChoice()))
-humanScore > compScore ? alert(`Human wins. Human: ${humanScore + 1} - Computer: ${compScore}`) : alert(`Computer wins. Human: ${humanScore} - Computer: ${compScore + 1}`)
+// playGame(playRound(getCompChoice(),getHumanChoice()))
+// humanScore > compScore ? alert(`Human wins. Human: ${humanScore + 1} - Computer: ${compScore}`) : alert(`Computer wins. Human: ${humanScore} - Computer: ${compScore + 1}`)
 
 
